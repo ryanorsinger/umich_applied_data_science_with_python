217a74ec39acfb6fe4fc4d1f32702fc392f8344e
RyanOrsinger <ryan.orsinger@gmail.com>
2019-04-02 18:23:22 -0500

    add drop columns

--- week3.py
+++ week3.py
@@ -8,6 +8,10 @@ energy = pd.read_excel(energy_url)
 # drops all the header and footer lines
 energy = energy.iloc[15:243]
 
-print(energy)
+energy = energy.drop(columns=['Unnamed: 0'])
+
+
+
+# print(energy.columns.values)
 
 # assign columns "Country", "Energy Supply", "Energy Supply per Capita", "% Renewable"
