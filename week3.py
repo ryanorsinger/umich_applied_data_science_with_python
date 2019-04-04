98eac2f311ed12675d1d335bd9f644ed224524ef
RyanOrsinger <ryan.orsinger@gmail.com>
2019-04-04 09:22:01 -0500

    Cleaned up kruft from excel file

--- week3.py
+++ week3.py
@@ -6,12 +6,21 @@ energy_url = "EnergyIndicators.xls"
 energy = pd.read_excel(energy_url)
 
 # drops all the header and footer lines
-energy = energy.iloc[15:243]
+energy = energy.iloc[17:243]
 
-energy = energy.drop(columns=['Unnamed: 0'])
+# drop unnecessary columns
+energy = energy.drop(columns=['Unnamed: 0', 'Unnamed: 2'])
 
+# rename columns of interest
+energy = energy.rename(columns = {
+    "Unnamed: 1": "Country", 
+    "Unnamed: 3": 'Energy Supply',
+    "Unnamed: 4": "Energy Supply per Capita",
+    "Unnamed: 5": "% Renewable"});
 
+energy = energy.set_index("Country")
 
-# print(energy.columns.values)
+energy['Energy Supply'] = energy['Energy Supply'] * 1000000
 
-# assign columns "Country", "Energy Supply", "Energy Supply per Capita", "% Renewable"
+print(energy.head())
+
